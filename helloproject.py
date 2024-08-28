68e737f6fa9a7963c92b1fc6e09243e67be4e47b
emiryildirimm <emir67118@gmail.com>
2024-08-28 12:02:28 +0300

    hello-message-update

---
+++ helloproject.py
@@ -0,0 +1,7 @@
+# ---------------------- hello everyone ---------------------
+
+# ---------------------- this is the hello page for project ----------------
+
+# ---------------------- in this project we'll create a BMI calculator -------------
+
+print("Hi there, welcome my BMI calculator")
