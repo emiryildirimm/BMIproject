add7871b4401e6bcfb1be7d73791db72362937e0
emiryildirimm <emir67118@gmail.com>
2024-08-29 08:32:36 +0300

    Last features added

--- BMIcalculator.py
+++ BMIcalculator.py
@@ -9,8 +9,22 @@ global value
 
 def button_click():
     global value
-    user_weight_value= int(user_weight.get())
-    user_height_value = float(user_height.get())**2
+    age = user_age.get()
+    weight = user_weight.get()
+    height = user_height.get()
+
+
+
+    if not age or not weight or not height:
+        messagebox.showerror("Error", "All fields must be filled out.")
+
+    try:
+        user_weight_value = int(user_weight.get())
+        user_height_value = float(user_height.get()) ** 2
+    except ValueError:
+        messagebox.showerror("Error", "Please enter valid numbers for weight and height.")
+        return
+
     value = user_weight_value / user_height_value
 
 
@@ -24,8 +38,6 @@ def button_click():
         messagebox.showinfo("BMI", f"Your BMI :{value} - Obese ")
     else:
         messagebox.showinfo("BMI", f"Your BMI :{value} - Extremely Obese ")
-
-
 
 root.title("BMI Calculator")
 root.configure(bg='white')
@@ -69,7 +81,5 @@ user_height_label.place(x=5, y=160)
 button = Button(root, text="Calculate",activebackground="blue", activeforeground="white", command=button_click)
 button.pack()
 button.place(x=195, y=210)
-
-
 
 root.mainloop()
